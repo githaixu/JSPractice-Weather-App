4c0c9038042fbcf7f8f3288a93edbdda4440c1c3
Jhin <40558041+githaixu@users.noreply.github.com>
2019-11-13 18:03:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const number = document.querySelector('#number');
 const description = document.querySelector('#description');
 const timezone = document.querySelector('#timezone');
 const domicon = document.querySelector('#icon');
+const number2 = document.querySelector('#number2');
 
 //windowがロードしたら緯度と経度を取得する
 window.addEventListener('load', function(e){
@@ -12,27 +13,27 @@ window.addEventListener('load', function(e){
   let geolocation = navigator.geolocation;
   //navigator.geolocationが動作する場合
   if(geolocation){
+    //tun getCurrentPosition function and pass in seccess and error callback function
+    geolocation.getCurrentPosition(success, error);
     //成功のコールバック関数の作成
     function success(position){
       //成功した場合緯度と経度を取得する
-      let crd = position.coords;
-      long = crd.longitude;
-      lat = crd.latitude;
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
       //現在位置の緯度と経度に基づいてAPIから気温などの情報を取得する
       const PROXY = 'https://cors-anywhere.herokuapp.com/';
       //CROS問題を解決するためにPROXYを設置
       const API = `${PROXY}https://api.darksky.net/forecast/3fcf9c5ab53a46503f8931f93a5ecc98/${lat},${long}?units=si&lang=ja&exclude=flags,hourly,daily`;
-      //fetchからpromiseが帰ってくる
+      //fetchでAPIにリクエストして　fetchからpromiseが帰ってくる
       fetch(API)
         .then(response => response.json())
         .then(json => {
-          const {temperature, summary, icon} = json.currently;
+          const {temperature, summary, icon, humidity} = json.currently;
           //DOMにAPIから取得した情報をセットする
           number.textContent = temperature;
           description.textContent = summary;
           timezone.textContent = json.timezone;
-          console.log(icon)
-          console.log(domicon);
+          number2.textContent = humidity*100;
           setIcon(icon, domicon)
         })
     }
@@ -40,9 +41,7 @@ window.addEventListener('load', function(e){
     function error(error){
       let code = error.code;
       let message = error.message;
-      console.warm(code + message);
     }
-    geolocation.getCurrentPosition(success, error)
   //navigator.geolocation が動作しない場合
   }else{
     alert('the geolocaton function is not work in this browser');
